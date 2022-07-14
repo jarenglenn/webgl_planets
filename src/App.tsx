5863298103596b759d76a487f9a5c6e6af314e7e
Jaren Glenn <jarenglenn@gmail.com>
2022-07-14 13:56:58 -0600

    converted hexasphere to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import Face from "./hexaspherejs/face";
 import Hexasphere from "./hexaspherejs/hexasphere";
+import Point from "./hexaspherejs/point";
+
+type Tile = {
+  centerPoint: Point;
+  boundary: Point[];
+  neighbors: Tile[];
+  neighborIds: string[];
+  faces: Face[];
+};
 
 export default function App() {
   const hexasphere = new Hexasphere(15, 5, 0.9);
+
+  const tile: Tile = hexasphere.tiles[1];
+  console.log(tile);
 
   return <h1>Hello, world!</h1>;
 }
