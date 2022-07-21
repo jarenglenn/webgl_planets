a2a27eaecba107eaa35eb0041c8900c3d8fe88f5
Jaren Glenn <jarenglenn@gmail.com>
2022-07-20 19:23:00 -0600

    cleaned up App.tsx - removed unused imports - removed unused Tile type - changed style of triangle declaration to be more readable

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import { OrbitControls } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
-import { BufferAttribute, BufferGeometry, Color, DoubleSide } from "three";
-import Face from "./hexaspherejs/face";
+import { BufferAttribute, BufferGeometry, Color } from "three";
 import Hexasphere from "./hexaspherejs/hexasphere";
-import Point from "./hexaspherejs/point";
 import { useMemo } from "react";
 import { calculateSurfaceNormal, pointingAwayFromOrigin } from "./hexaspherejs/tile";
-
-type Tile = {
-  centerPoint: Point;
-  boundary: Point[];
-  neighbors: Tile[];
-  neighborIds: string[];
-  faces: Face[];
-};
 
 export default function App() {
   const { scene } = useThree();
@@ -21,8 +11,8 @@ export default function App() {
 
   var hexasphere = new Hexasphere(15, 3, 0.8);
 
-  const triangles = hexasphere.tiles.map((tile) => {
-    return tile.faces.map((face) => {
+  const triangles = hexasphere.tiles.map((tile) =>
+    tile.faces.map((face) => {
       const normalVector = calculateSurfaceNormal(
         face.points[0],
         face.points[1],
@@ -31,16 +21,15 @@ export default function App() {
 
       const flipFactor = pointingAwayFromOrigin(face.centroid!, normalVector) ? -1 : 1;
 
-      return face.points.map((point) => {
-        return [point.x, point.y, point.z].map((coordinate) => coordinate * flipFactor);
-      });
-    });
-  });
+      return face.points.map((point) =>
+        [point.x, point.y, point.z].map((coordinate) => coordinate * flipFactor)
+      );
+    })
+  );
 
   let vertices = useMemo(() => new Float32Array(triangles.flat(3)), [triangles]);
 
   const hexasphereGeometry = new BufferGeometry();
-
   hexasphereGeometry.setAttribute("position", new BufferAttribute(vertices, 3));
 
   const geo = useMemo(() => {
