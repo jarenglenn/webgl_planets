0b0013c2177b8c612bb87f18a1ac7a1bcbb14f93
Jaren Glenn <jarenglenn@gmail.com>
2022-07-27 11:10:38 -0600

    merge tile geometries

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,53 @@
-import { OrbitControls } from "@react-three/drei";
+import { useMemo } from "react";
 import { useThree } from "@react-three/fiber";
-import {
-  BufferGeometry,
-  Color,
-  Float32BufferAttribute,
-  Mesh,
-  MeshStandardMaterial,
-} from "three";
+import { OrbitControls } from "@react-three/drei";
+import { BufferGeometry, Color, Float32BufferAttribute } from "three";
+import { mergeBufferGeometries } from "three/examples/jsm/utils/BufferGeometryUtils";
 import Hexasphere from "./hexaspherejs/hexasphere";
 
 export default function App() {
   const { scene } = useThree();
   scene.background = new Color("#f1e3bb");
 
-  var hexasphere = new Hexasphere(5, 5, 1);
+  var hexasphere = useMemo(() => new Hexasphere(5, 10, 1), []);
 
-  for (const tile of hexasphere.tiles) {
-    let vertices = [];
-    let indices = [];
+  const hexasphereGeometry = useMemo(() => {
+    const tileGeometries: BufferGeometry[] = [];
 
-    const geometry = new BufferGeometry();
+    for (const tile of hexasphere.tiles) {
+      let vertices = [];
+      let indices = [];
 
-    for (const boundaryPoint of tile.boundary) {
-      vertices.push(boundaryPoint.x, boundaryPoint.y, boundaryPoint.z);
+      const geometry = new BufferGeometry();
+
+      for (const boundaryPoint of tile.boundary) {
+        vertices.push(boundaryPoint.x, boundaryPoint.y, boundaryPoint.z);
+      }
+
+      indices.push(0, 1, 2, 0, 2, 3, 0, 3, 4);
+
+      if (vertices.length > 15) {
+        indices.push(0, 4, 5);
+      }
+
+      geometry.setAttribute("position", new Float32BufferAttribute(vertices, 3, false));
+      geometry.setIndex(indices);
+      geometry.computeVertexNormals();
+
+      tileGeometries.push(geometry);
     }
 
-    indices.push(0, 1, 2, 0, 2, 3, 0, 3, 4);
-
-    if (vertices.length > 15) {
-      indices.push(0, 4, 5);
-    }
-
-    geometry.setAttribute("position", new Float32BufferAttribute(vertices, 3, false));
-    geometry.setIndex(indices);
-    geometry.computeVertexNormals();
-
-    var mesh = new Mesh(geometry, new MeshStandardMaterial({ color: 0x87ceeb }));
-    scene.add(mesh);
-  }
+    const mergedGeometry = mergeBufferGeometries(tileGeometries);
+    return mergedGeometry;
+  }, [hexasphere]);
 
   return (
     <group>
       <OrbitControls />
-      {/* <mesh geometry={geo}>
-        <meshStandardMaterial color="#87ceeb" />
-      </mesh> */}
-      <pointLight position={[50, 50, 10]} />
+      <mesh geometry={hexasphereGeometry}>
+        <meshPhysicalMaterial color="#87ceeb" />
+      </mesh>
+      <pointLight position={[100, 50, 10]} intensity={0.5} />
       <ambientLight intensity={0.2} />
     </group>
   );
