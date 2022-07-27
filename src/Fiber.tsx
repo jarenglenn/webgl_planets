8683ea515806d73a57394e782efaa1d118aed6e5
Jaren Glenn <jarenglenn@gmail.com>
2022-07-27 15:21:29 -0600

    inside/outside hexagons - todo: sides

--- src/Fiber.tsx
+++ src/Fiber.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
-import { BufferGeometry, Float32BufferAttribute } from "three";
+import { BufferGeometry, DoubleSide, Float32BufferAttribute } from "three";
 import { mergeBufferGeometries } from "three/examples/jsm/utils/BufferGeometryUtils";
 import Hexasphere from "./hexaspherejs/hexasphere";
 import { HexasphereArgs } from "./App";
@@ -8,6 +8,8 @@ import { HexasphereArgs } from "./App";
 interface Props {
   hexasphereArgs: typeof HexasphereArgs;
 }
+
+const depthRatio = 1.5;
 
 export default function Fiber(props: Props) {
   const hexasphere = useMemo(
@@ -24,19 +26,50 @@ export default function Fiber(props: Props) {
     const tileGeometries: BufferGeometry[] = [];
 
     for (const tile of hexasphere.tiles) {
-      let vertices = [];
-      let indices = [];
+      let vertices: number[] = [];
+      let indices: number[] = [];
 
       const geometry = new BufferGeometry();
 
+      // Inside points
       for (const boundaryPoint of tile.boundary) {
         vertices.push(boundaryPoint.x, boundaryPoint.y, boundaryPoint.z);
       }
 
-      indices.push(0, 1, 2, 0, 2, 3, 0, 3, 4);
+      // Outside points
+      for (const boundaryPoint of tile.boundary) {
+        vertices.push(
+          boundaryPoint.x * depthRatio,
+          boundaryPoint.y * depthRatio,
+          boundaryPoint.z * depthRatio
+        );
+      }
 
-      if (vertices.length > 15) {
+      // Inside triangles
+
+      // prettier-ignore
+      indices.push(
+        0, 1, 2,
+        0, 2, 3,
+        0, 3, 4,
+      );
+
+      let outsideIndex = 5;
+
+      if (vertices.length > 30) {
         indices.push(0, 4, 5);
+        outsideIndex++;
+      }
+
+      // prettier-ignore
+      indices.push(
+        outsideIndex, outsideIndex + 1, outsideIndex + 2,
+        outsideIndex, outsideIndex + 2, outsideIndex + 3,
+        outsideIndex, outsideIndex + 3, outsideIndex + 4,
+      );
+
+      if (vertices.length > 30) {
+        indices.push(outsideIndex, outsideIndex + 4, outsideIndex + 5);
       }
 
       geometry.setAttribute("position", new Float32BufferAttribute(vertices, 3, false));
