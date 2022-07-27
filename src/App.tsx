30646209ce13ff3ee4213dcaf63d8afdecf15f92
Jaren Glenn <jarenglenn@gmail.com>
2022-07-27 01:12:21 -0600

    lol rewrote the entire thing to generate properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,50 @@
 import { OrbitControls } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
-import { BufferGeometry, Color, DoubleSide, Vector3 } from "three";
+import {
+  BufferGeometry,
+  Color,
+  Float32BufferAttribute,
+  Mesh,
+  MeshStandardMaterial,
+} from "three";
 import Hexasphere from "./hexaspherejs/hexasphere";
-import { useMemo } from "react";
 
 export default function App() {
   const { scene } = useThree();
   scene.background = new Color("#f1e3bb");
 
-  var hexasphere = new Hexasphere(5, 1, 1);
+  var hexasphere = new Hexasphere(5, 5, 1);
 
-  const vertices = hexasphere.tiles
-    .map((tile) =>
-      tile.boundary.map((point) => {
-        return new Vector3(point.x, point.y, point.z);
-      })
-    )
-    .flat();
+  for (const tile of hexasphere.tiles) {
+    let vertices = [];
+    let indices = [];
 
-  const geo = useMemo(() => {
     const geometry = new BufferGeometry();
-    geometry.setFromPoints(vertices);
+
+    for (const boundaryPoint of tile.boundary) {
+      vertices.push(boundaryPoint.x, boundaryPoint.y, boundaryPoint.z);
+    }
+
+    indices.push(0, 1, 2, 0, 2, 3, 0, 3, 4);
+
+    if (vertices.length > 15) {
+      indices.push(0, 4, 5);
+    }
+
+    geometry.setAttribute("position", new Float32BufferAttribute(vertices, 3, false));
+    geometry.setIndex(indices);
     geometry.computeVertexNormals();
-    return geometry;
-  }, [vertices]);
 
-  // var blob = new Blob([hexasphere.toObj()], { type: "text/plain;charset=utf-8" });
-
-  // downloadFile(blob, "hexasphere.obj");
+    var mesh = new Mesh(geometry, new MeshStandardMaterial({ color: 0x87ceeb }));
+    scene.add(mesh);
+  }
 
   return (
     <group>
       <OrbitControls />
-      <mesh geometry={geo}>
-        <meshStandardMaterial color="#87ceeb" side={DoubleSide} />
-      </mesh>
+      {/* <mesh geometry={geo}>
+        <meshStandardMaterial color="#87ceeb" />
+      </mesh> */}
       <pointLight position={[50, 50, 10]} />
       <ambientLight intensity={0.2} />
     </group>
