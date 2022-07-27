e5858cc5e967a99a52d5878984933f7efc1f9ebc
Jaren Glenn <jarenglenn@gmail.com>
2022-07-27 00:27:52 -0600

    vertices correctly in place - todo: create proper faces

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,42 @@
 import { OrbitControls } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
-import { BufferAttribute, BufferGeometry, Color } from "three";
+import { BufferGeometry, Color, DoubleSide, Vector3 } from "three";
 import Hexasphere from "./hexaspherejs/hexasphere";
 import { useMemo } from "react";
-import { calculateSurfaceNormal, pointingAwayFromOrigin } from "./hexaspherejs/tile";
 
 export default function App() {
   const { scene } = useThree();
   scene.background = new Color("#f1e3bb");
 
-  var hexasphere = new Hexasphere(15, 3, 0.8);
+  var hexasphere = new Hexasphere(5, 1, 1);
 
-  const triangles = hexasphere.tiles.map((tile) =>
-    tile.faces.map((face) => {
-      const normalVector = calculateSurfaceNormal(
-        face.points[0],
-        face.points[1],
-        face.points[2]
-      );
-
-      const flipFactor = pointingAwayFromOrigin(face.centroid!, normalVector) ? -1 : 1;
-
-      return face.points.map((point) =>
-        [point.x, point.y, point.z].map((coordinate) => coordinate * flipFactor)
-      );
-    })
-  );
-
-  let vertices = useMemo(() => new Float32Array(triangles.flat(3)), [triangles]);
-
-  const hexasphereGeometry = new BufferGeometry();
-  hexasphereGeometry.setAttribute("position", new BufferAttribute(vertices, 3));
+  const vertices = hexasphere.tiles
+    .map((tile) =>
+      tile.boundary.map((point) => {
+        return new Vector3(point.x, point.y, point.z);
+      })
+    )
+    .flat();
 
   const geo = useMemo(() => {
     const geometry = new BufferGeometry();
-    const positionNumComponents = 3;
-    geometry.setAttribute(
-      "position",
-      new BufferAttribute(new Float32Array(vertices), positionNumComponents)
-    );
+    geometry.setFromPoints(vertices);
+    geometry.computeVertexNormals();
     return geometry;
   }, [vertices]);
+
+  // var blob = new Blob([hexasphere.toObj()], { type: "text/plain;charset=utf-8" });
+
+  // downloadFile(blob, "hexasphere.obj");
 
   return (
     <group>
       <OrbitControls />
       <mesh geometry={geo}>
-        <meshStandardMaterial color="red" />
+        <meshStandardMaterial color="#87ceeb" side={DoubleSide} />
       </mesh>
-      <pointLight position={[10, 5, 10]} />
+      <pointLight position={[50, 50, 10]} />
+      <ambientLight intensity={0.2} />
     </group>
   );
 }
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,4 @@
+import Point from "./hexaspherejs/point";
 import Tile from "./hexaspherejs/tile";
 
 // https://www.khronos.org/opengl/wiki/Calculating_a_Surface_Normal
@@ -22,3 +23,24 @@ export function calculateTileSurfaceNormal(tile: Tile) {
 
 export const repeatArray = (arr: any[], repeats: number) =>
   Array.from({ length: repeats }, () => arr).flat();
+
+export function downloadFile(blob: any, filename: string): void {
+  const objectUrl: string = URL.createObjectURL(blob);
+  const a: HTMLAnchorElement = document.createElement("a") as HTMLAnchorElement;
+
+  a.href = objectUrl;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+
+  document.body.removeChild(a);
+  URL.revokeObjectURL(objectUrl);
+}
+
+export const findCentroid = (triangle: Point[]) => {
+  let x = (triangle[0].x + triangle[1].x + triangle[2].x) / 3;
+  let y = (triangle[0].y + triangle[1].y + triangle[2].y) / 3;
+  let z = (triangle[0].z + triangle[1].z + triangle[2].z) / 3;
+
+  return new Point(x, y, z);
+};
